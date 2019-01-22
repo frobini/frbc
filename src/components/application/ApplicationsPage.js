9418690b81cd607cfb110c35fdaa0f90c81432c1
Fabrice Robini <frobini@thefcomputer.local>
2019-01-22 15:45:07 +0100

    [init] Basic full working blank app

---
+++ src/components/application/ApplicationsPage.js
@@ -0,0 +1,17 @@
+import React,  {PropTypes} from 'react';
+
+class  ApplicationsPage extends React.Component{
+
+  render(){
+    return (
+      <div>
+        <h1>Applications</h1>
+      </div>
+    );
+  }
+
+}
+
+export default ApplicationsPage;
+
+
